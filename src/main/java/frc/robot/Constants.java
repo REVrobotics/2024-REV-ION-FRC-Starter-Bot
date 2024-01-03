69c4008bfad4e95bb5441e34b41370e5af2b73a6
Orion DeYoe <38528765+OrionSword@users.noreply.github.com>
2024-01-03 17:02:19 -0600

    Copied in and modified arm subsystem from 2023

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,10 +6,12 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import frc.lib.PIDGains;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
@@ -24,6 +26,29 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class Arm {
+        public static final int kArmCanId = 2;
+        public static final boolean kArmInverted = true;
+        public static final int kCurrentLimit = 40;
+
+        public static final double kSoftLimitReverse = -1.15;
+        public static final double kSoftLimitForward = 0.0;
+
+        public static final double kArmGearRatio = (1.0/25.0) * (28.0 / 50.0) * (16.0 / 64.0); 
+        public static final double kPositionFactor = kArmGearRatio * 2.0 * Math.PI; //multiply SM value by this number and get arm position in radians
+        public static final double kVelocityFactor = kArmGearRatio * 2.0 * Math.PI / 60.0;
+        public static final double kArmFreeSpeed = 5676.0 * kVelocityFactor;
+        public static final double kArmZeroCosineOffset = 1.342; //radians to add to converted arm position to get real-world arm position (starts at ~76.9deg angle)
+        public static final ArmFeedforward kArmFeedforward = new ArmFeedforward(0.0, 0.0, 12.0/kArmFreeSpeed, 0.0);
+        public static final PIDGains kArmPositionGains = new PIDGains(0.1, 0.0, 0.0);
+        public static final TrapezoidProfile.Constraints kArmMotionConstraint = new TrapezoidProfile.Constraints(1.0, 2.0);
+
+        public static final double kHomePosition = 0.0;
+        public static final double kScoringPosition = -0.10;
+        public static final double kIntakePosition = -1.11;
+  }
+
+
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
@@ -35,9 +60,9 @@ public final class Constants {
     public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    public static final double kTrackWidth = Units.inchesToMeters(21.5);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    public static final double kWheelBase = Units.inchesToMeters(21.5);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -52,15 +77,15 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 3;
-    public static final int kRearLeftDrivingCanId = 7;
-    public static final int kFrontRightDrivingCanId = 1;
-    public static final int kRearRightDrivingCanId = 5;
+    public static final int kFrontLeftDrivingCanId = 11;
+    public static final int kRearLeftDrivingCanId = 13;
+    public static final int kFrontRightDrivingCanId = 15;
+    public static final int kRearRightDrivingCanId = 17;
 
-    public static final int kFrontLeftTurningCanId = 4;
-    public static final int kRearLeftTurningCanId = 8;
-    public static final int kFrontRightTurningCanId = 2;
-    public static final int kRearRightTurningCanId = 6;
+    public static final int kFrontLeftTurningCanId = 10;
+    public static final int kRearLeftTurningCanId = 12;
+    public static final int kFrontRightTurningCanId = 14;
+    public static final int kRearRightTurningCanId = 16;
 
     public static final boolean kGyroReversed = false;
   }
